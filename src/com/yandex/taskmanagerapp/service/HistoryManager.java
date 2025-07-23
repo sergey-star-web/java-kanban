fb8c5085a6b767fdba586bd0344c4e6fcbc8fc65
Sergey <we_ew_2015@mail.ru.ru>
2025-07-24 02:31:59 +0300

    Исправление ошибок от автотеста №4

--- src/com/yandex/taskmanagerapp/service/HistoryManager.java
+++ src/com/yandex/taskmanagerapp/service/HistoryManager.java
@@ -2,11 +2,12 @@ package com.yandex.taskmanagerapp.service;
 
 import com.yandex.taskmanagerapp.model.Task;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
