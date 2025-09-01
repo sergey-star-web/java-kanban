944cf69ab44afb2a03e0de9499aa67b16cbb1c44
Sergey <we_ew_2015@mail.ru.ru>
2025-09-02 01:50:35 +0300

    Реализация ФЗ 9 спринт №1

---
+++ src/com/yandex/taskmanagerapp/api/HttpTaskServer.java
@@ -0,0 +1,167 @@
+package com.yandex.taskmanagerapp.api;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
+import com.yandex.taskmanagerapp.constants.Constant;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.yandex.taskmanagerapp.enums.Status;
+import com.yandex.taskmanagerapp.exceptions.ManagerSaveException;
+import com.yandex.taskmanagerapp.model.Epic;
+import com.yandex.taskmanagerapp.model.Subtask;
+import com.yandex.taskmanagerapp.model.Task;
+import com.yandex.taskmanagerapp.service.FileBackedTaskManager;
+import com.yandex.taskmanagerapp.service.InMemoryTaskManager;
+import com.yandex.taskmanagerapp.service.Managers;
+import com.yandex.taskmanagerapp.service.TaskManager;
+
+public class HttpTaskServer {
+    private static HttpClient httpClient;
+    private static HttpServer httpServer;
+    private static TaskManager taskManager = Managers.getMemoryTaskManager();
+
+    public static void main(String[] args) {
+
+        List<Task> tasksList;
+        List<Task> historyTasks;
+        Task getTask;
+        Subtask getSubTask;
+        Epic getEpic;
+        File file;
+        File fileTestLoad;
+
+        Task task1 = new Task("first_task", "non desc", Status.NEW, 35, "2025-08-01 14:15:30");
+        Task task2 = new Task("second_task", "non desc", Status.NEW, 45, "2025-08-01 10:00:00");
+        Epic epic1 = new Epic("first_epic", "non desc");
+        Subtask sub1 = new Subtask("first_sub", "non desc", Status.NEW, 60, "2025-08-04 10:30:00");
+        Subtask sub2 = new Subtask("second_sub", "non desc", Status.NEW, 40, "2025-08-03 13:00:00");
+        Epic epic2 = new Epic("second_epic", "non desc");
+        Subtask sub3 = new Subtask("thirst_sub", "non desc", Status.NEW, 50, "2025-08-03 15:35:00");
+        Subtask sub4 = new Subtask("four_sub", "non desc", Status.NEW, 100, null);
+        Subtask sub5 = new Subtask("five_sub", "non desc", Status.NEW, 80, "2025-08-03 12:00:00");
+
+        taskManager.addTask(task1);
+        taskManager.addTask(task2);
+        taskManager.addEpic(epic1);
+        taskManager.addEpic(epic2);
+        sub1.setIdEpic(epic1.getId());
+        sub2.setIdEpic(epic1.getId());
+        sub3.setIdEpic(epic2.getId());
+        sub4.setIdEpic(epic1.getId());
+        sub5.setIdEpic(epic2.getId());
+        taskManager.addSubtask(sub1);
+        taskManager.addSubtask(sub2);
+        taskManager.addSubtask(sub3);
+        taskManager.addSubtask(sub4);
+        taskManager.addSubtask(sub5);
+
+        try {
+            start();
+
+            URI uri = URI.create("http://localhost:" + Constant.port + "/epics/3/subtasks");
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            handleRequest(request, response);
+
+            System.out.println("Сервер запущен на порту " + Constant.port);
+            //stop();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void start() {
+        init(8080, 5, 5);
+        httpServer.start();
+    }
+
+    public static void stop() {
+        httpServer.stop(0);
+    }
+
+    public static void handleRequest(HttpRequest request, HttpResponse response) {
+        // Получаем метод запроса (GET, POST и DELETE)
+        String method = request.method();
+        String[] path = request.uri().getPath().split("/");
+        String pathMap = path[1];
+        Integer pathId = null;
+        if (path.length > 2) {
+            pathId = Integer.parseInt(path[2]);
+        }
+        // Обработаем каждый метод запроса
+        if (method.equals("GET")) {
+            if (pathMap.equals("tasks")) {
+                if (pathId != null) {
+                    taskManager.getTask(pathId);
+                    System.out.println(taskManager.getTask(pathId));
+                } else {
+                    taskManager.getTasks();
+                    System.out.println(taskManager.getTasks());
+                }
+            }
+
+            if (pathMap.equals("subtasks")) {
+                if (pathId != null) {
+                    taskManager.getTask(pathId);
+                    System.out.println(taskManager.getTask(pathId));
+                } else {
+                    taskManager.getTasks();
+                    System.out.println(taskManager.getSubtasks());
+                }
+            }
+
+            if (pathMap.equals("epics")) {
+                String pathS = null;
+                if (path.length > 3) {
+                    pathS = path[3];
+                }
+                if (pathS != null && pathS.equals("subtasks")) {
+                    taskManager.getSubtasksInEpic(pathId);
+                    System.out.println(taskManager.getSubtasksInEpic(pathId));
+                } else if (pathId != null) {
+                    taskManager.getTask(pathId);
+                    System.out.println(taskManager.getTask(pathId));
+                } else {
+                    taskManager.getTasks();
+                    System.out.println(taskManager.getEpics());
+                }
+            }
+
+        } else if (method.equals("POST")) {
+            String responseBody = "Hello, world!";
+        } else if (method.equals("DELETE")) {
+            String responseBody = "Hello, world!";
+        } else {
+            System.out.println("Метод: " + method + " не обрабатывается");
+        }
+    }
+
+    public static void init(Integer port, Integer backlog, Integer maxConnections) {
+        try {
+            httpClient = HttpClient.newHttpClient();
+            httpServer = HttpServer.create();
+            httpServer.bind(new InetSocketAddress(port), backlog);
+            // Настройка максимального количества подключений
+            ExecutorService executor = Executors.newFixedThreadPool(maxConnections);
+            httpServer.setExecutor(executor);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/com/yandex/taskmanagerapp/constants/Constant.java
+++ src/com/yandex/taskmanagerapp/constants/Constant.java
@@ -4,4 +4,6 @@ import java.time.format.DateTimeFormatter;
 
 public class Constant {
     public static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final Integer port = 8080;
+    public static final String host = "http://localhost:" + port + "/";
 }
